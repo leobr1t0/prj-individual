94105f8e691c24bdacbd84d0a2dad2ba73dd68f3
leobr1t0 <leonardo.oliveira@sptech.school>
2022-11-28 20:29:11 -0300

    Ajustes finais

--- site/src/models/quizModel.js
+++ site/src/models/quizModel.js
@@ -16,7 +16,6 @@ function listarUsuario(id) {
     console.log("ACESSEI O QUIZ MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucao = `
     SELECT * FROM tbPontuacao 
-	JOIN tbQuiz ON idQuiz = fkQuiz
 		WHERE fkUsuario = ${id};
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
